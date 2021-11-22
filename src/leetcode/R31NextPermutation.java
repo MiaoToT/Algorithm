8ac2f806a06fad51fddbe0f4e2143430618659ec
喵粮都输光了 <na@kuukokawaii.com>
2021-11-22 23:22:56 +0800

    feat(master):
    [skip ci]

---
+++ src/leetcode/R31NextPermutation.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * @author 喵粮都输光了
+ * @date 2021/11/22
+ * @description 31.下一个排列 https://leetcode-cn.com/problems/next-permutation/
+ */
+public final class R31NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+
+    }
+
+}
